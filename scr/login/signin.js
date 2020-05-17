c761ccedb98db46549d824e0f91f1e1faf287484
sanchez1535 <65515850+sanchez1535@users.noreply.github.com>
2020-05-17 19:58:33 -0400

    Create signin.js

---
+++ scr/login/signin.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+
+
+import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Expo,Platform, StyleSheet, Image, Button, TouchableHighlight} from 'react-native';
+
+
+class Signin extends React.Component {
+
+
+  render() {
+     
+    return (
+      <KeyboardAvoidingView style={styles.containerView} behavior="padding">
+
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+
+ <View style={styles.textContainer}>
+
+</View>
+
+             <TextInput placeholder="Email" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+            
+            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+
+             <View style={styles.textContainer}>
+            <TouchableHighlight
+              onPress={() => this.props.navigation.navigate('Workout')}
+            >
+           <View style={styles.touchableButton1}>
+                        <Text style={styles.buttonText}>
+                            CONTINUE
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+
+
+
+
+
+ <View style={styles.textContainer}>
+
+
+ <Button
+          title="DONT HAVE AN ACCOUNT ? SIGN UP "
+          onPress={() => this.props.navigation.navigate('Sign')}
+        />
+          
+          </View>
+
+
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    );
+  }
+
+}
+
+
+
+const styles = StyleSheet.create({
+
+containerView: {
+  flex: 1,
+},
+loginScreenContainer: {
+  flex: 1,
+},
+textContainer: {
+  flex:0.5,
+  justifyContent: 'center',
+  alignItems: 'center',
+  },
+loginFormView: {
+  flex: 1
+},
+loginFormTextInput: {
+  height: 43,
+  fontSize: 14,
+  borderRadius: 5,
+  borderWidth: 1,
+  borderColor: '#eaeaea',
+  backgroundColor: '#fafafa',
+  paddingLeft: 10,
+  marginLeft: 15,
+  marginRight: 15,
+  marginTop: 5,
+  marginBottom: 5,
+
+},
+touchableButton1: {
+        backgroundColor: 'blue',
+        height: 40,
+        width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+        marginBottom: 50,
+        borderRadius: 10,
+        borderWidth: 1, 
+    },
+ buttonText: {
+        color: 'white',
+        fontSize: 14,
+        fontWeight:'bold',
+ }
+});
+
+export default Signin;
